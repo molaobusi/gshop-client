5f5536ba9a4529b53626700c0e5d37e844027b78
robinsen_global <robinsen_global@126.com>
2018-08-25 14:10:42 +0800

    优化部分bug

--- src/main.js
+++ src/main.js
@@ -6,10 +6,15 @@ import {Button} from 'mint-ui'
 import App from './App'
 import router from './router/index'
 import store from './store/index'
+import VueLazyload from 'vue-lazyload'
 
 import './mock/mockServer' // 加载mockServer即可
+import './filters/index' // 加载filters即可
 
 Vue.component(Button.name, Button);
+Vue.use(VueLazyload, {
+  loading: '../static/images/loading.gif',
+});
 
 (() => new Vue({
     el: '#app',
--- src/router/index.js
+++ src/router/index.js
@@ -5,12 +5,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import Home from '../pages/home/Home'
-import Order from '../pages/order/Order'
-import Search from '../pages/search/Search'
-import Person from '../pages/person/Person'
-import Login from '../pages/login/Login'
-import Shop from '../pages/shop/Shop'
+// import Home from '../pages/home/Home'
+// import Order from '../pages/order/Order'
+// import Search from '../pages/search/Search'
+// import Person from '../pages/person/Person'
+// import Login from '../pages/login/Login'
+// import Shop from '../pages/shop/Shop'
+
+// 路由组件懒加载
+const Home = () => import('../pages/home/Home.vue')
+const Order = () => import('../pages/order/Order.vue')
+const Search = () => import('../pages/search/Search.vue')
+const Person = () => import('../pages/person/Person')
+const Login = () => import('../pages/login/Login.vue')
+const Shop = () => import('../pages/shop/Shop.vue')
 
 import ShopGoods from '../pages/shop/shopGoods/ShopGoods'
 import ShopRatings from '../pages/shop/shopRatings/ShopRatings'
